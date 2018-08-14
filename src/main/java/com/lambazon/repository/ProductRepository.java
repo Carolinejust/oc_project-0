2febc55a8b8e1030ad47a66fdee3d3813e966ab7
Caroline <carolinejust@hotmail.com>
2018-08-14 12:43:01 +0200

    Add 2 Products (Procesor and Keypad) in method generateProductData()

--- src/main/java/com/lambazon/repository/ProductRepository.java
+++ src/main/java/com/lambazon/repository/ProductRepository.java
@@ -22,6 +22,8 @@ public class ProductRepository {
 		products.put(++id, new Product(id, 30, 69.99, "JVC HAFX8R Headphone", "Riptidz, In-Ear"));
 		products.put(++id, new Product(id, 40, 32.50, "VTech CS6114 DECT 6.0", "Cordless Phone"));
 		products.put(++id, new Product(id, 50, 895.00, "NOKIA OEM BL-5J", "Cell Phone "));
+		products.put(++id, new Product(id, 60, 10.99, "Logitech K120", "Keypad "));
+		products.put(++id, new Product(id, 70, 919.59, "Intel Core i9 I9-7900X", "Processor"));
 	}
 	
 	public List<Product> products() {
